c3029444b3c58ee9b1e43f155dc74d4ef000e16f
0xSunil <sunday7637@gmail.com>
2023-06-22 13:22:40 +0530

    added loading screen

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -24,31 +24,31 @@ const Sidebar = () => {
         <ul className="list-reset flex flex-col gap-4">
           <li
             onClick={() => handleButtonClick(0)}
-            className={`px-4 py-2 text-2xl font-bold hover:bg-yellow-300 ${getButtonColor(
+            className={`px-4 py-2 text-2xl font-bold hover:bg-black rounded-2xl ${getButtonColor(
               0
             )}`}
           >
-            <Link href="/bitcoin" className="text-gray-300 hover:text-black ">
+            <Link href="/bitcoin" className="text-white  ">
               Bitcoin
             </Link>
           </li>
           <li
             onClick={() => handleButtonClick(1)}
-            className={`px-4 py-2 text-2xl font-bold hover:bg-yellow-300 ${getButtonColor(
+            className={`px-4 py-2 text-2xl font-bold hover:bg-black rounded-2xl ${getButtonColor(
               1
             )}`}
           >
-            <Link href="/ethereum" className="text-gray-300 hover:text-black ">
+            <Link href="/ethereum" className="text-white  ">
               Ethereum
             </Link>
           </li>
           <li
             onClick={() => handleButtonClick(2)}
-            className={`px-4 py-2 text-2xl font-bold hover:bg-yellow-300 ${getButtonColor(
+            className={`px-4 py-2 text-2xl font-bold hover:bg-black rounded-2xl ${getButtonColor(
               2
             )}`}
           >
-            <Link href="/bnb" className="text-gray-300 hover:text-black ">
+            <Link href="/bnb" className="text-white  ">
               BNB
             </Link>
           </li>
